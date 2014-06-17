2d825dc700c8a35db931d2b852934c33332f9207
Fabian de Laender <fabian@faapz.nl>
2014-06-17 19:05:47 +0200

    Minor changes

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-midway
-======
+# midway
 
 simplified responsive elements centering
+
+*Originally inspired by [Brandon Jacoby](https://github.com/BrandonJacoby/Midway.js)*
+
+*Rewritten natively by [Fabian de Laender](https://github.com/FaaPz/midway)*
+
+### Usage
+
+``` html
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>midway example</title>
+  <script type="text/javascript" src="midway.min.js"></script>
+</head>
+<body>
+  <div class="midway-horizontal midway-vertical"></div>
+</body>
+</html>
+```
+
+### Changelog
+
+##### v1.0.1
+  - Minor changes
+
+##### v1.0.0
+  - First version
--- midway.js
+++ midway.js
@@ -1,4 +1,4 @@
-/* midway - simplified responsive elements centering v1.0.0
+/* midway - simplified responsive elements centering v1.0.1
  * Crafted in Finland by a Dutchman called Fabian de Laender
  * Copyright (c) 2014 Fabian de Laender; Licensed MIT
  */
@@ -6,16 +6,17 @@ function _initMidway() {
   'use strict';
 
   var centerHorizontal = document.querySelectorAll('.midway-horizontal'),
-        centerVertical = document.querySelectorAll('.midway-vertical');
+        centerVertical = document.querySelectorAll('.midway-vertical'),
+                     i = 0;
 
-  for( var i = 0; i < centerHorizontal.length; i++ ) {
+  for( i = 0; i < centerHorizontal.length; i++ ) {
     centerHorizontal[i].style.marginLeft = -centerHorizontal[i].offsetWidth / 2 + 'px';
     centerHorizontal[i].style.display    = 'inline';
     centerHorizontal[i].style.position   = 'absolute';
     centerHorizontal[i].style.left       = '50%';
   }
 
-  for( var i = 0; i < centerVertical.length; i++ ) {
+  for( i = 0; i < centerVertical.length; i++ ) {
     centerVertical[i].style.marginTop = -centerVertical[i].offsetWidth / 2 + 'px';
     centerVertical[i].style.display   = 'inline';
     centerVertical[i].style.position  = 'absolute';
@@ -25,8 +26,8 @@ function _initMidway() {
 
 window.onload = function() {
   _initMidway();
-}
+};
 
 window.onresize = function() {
   _initMidway();
-}
+};
